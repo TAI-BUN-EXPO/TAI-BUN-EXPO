f408f876a6d58c4a93862d9f6cca67b0fbc344ee
Fafofafoy <ihcaoe@gmail.com>
2019-10-28 20:37:19 +0800

    Ka bangtsi

--- src/tshingtuann.js
+++ src/tshingtuann.js
@@ -5,19 +5,19 @@ export const tshingtuann = [
 	hangbok: [
 	    {
 		    too: require('./too/20191101 1930 開幕陳奕齊.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/513428939453083/',
 		},
 	    {
 		    too: require('./too/20191102 1430 來去鹽埕埔大溝頂行踏.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/916503622065133/',
 		},
 	    {
 		    too: require('./too/20191102 2000 大俠梅花鹿.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/797295507394407/',
 		},
 	    {
 		    too: require('./too/20191103 1500 古意唸歌團.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/427959851182930/',
 		},
 	],
   },
@@ -26,19 +26,19 @@ export const tshingtuann = [
 	hangbok: [
 	    {
 		    too: require('./too/20191109 1430 台南妹仔周定邦.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/667029807118732/',
 		},
 	    {
 		    too: require('./too/20191110 0900 來去旗津行踏.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/423415854960841/',
 		},
 	    {
 		    too: require('./too/20191110 1430 罐頭塔DIY.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/1325728987586438/',
 		},
 	    {
 		    too: require('./too/20191110 2000 拍謝少年維尼.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/745838035830123/',
 		},
 	],
   },
@@ -47,19 +47,19 @@ export const tshingtuann = [
 	hangbok: [
 	    {
 		    too: require('./too/20191116 1000 林美美.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/693206834506951/',
 		},
 	    {
 		    too: require('./too/20191116 1300 台語市仔.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/400708907526469/',
 		},
 	    {
 		    too: require('./too/20191116 1930 魚仔林.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/2342940019368463/',
 		},
 	    {
 		    too: require('./too/20191117 1430 王昭華.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/373326756955137/',
 		},
 	],
   },
@@ -67,16 +67,20 @@ export const tshingtuann = [
 	sikan: "11.23 Sat. - 11.24 Sun.",
 	hangbok: [
 	    {
+		    too: require('./too/20191123 1100 趙子賢創作個展.jpg'),
+		    bangtsi: 'https://www.facebook.com/events/483413442212897/',
+		},
+	    {
 		    too: require('./too/20191123 1430 你今仔日kám有講台語.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/408043233247829/',
 		},
 	    {
 		    too: require('./too/20191124 0900 來去柴山.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/483413442212897/',
 		},
 	    {
 		    too: require('./too/20191124 1430 港台垃圾話垃圾講.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/2305289829746135/',
 		},
 	],
   },
@@ -85,11 +89,15 @@ export const tshingtuann = [
 	hangbok: [
 	    {
 		    too: require('./too/20191130 0900 來去哈馬星行踏.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/901612790220586/',
+		},
+	    {
+		    too: require('./too/20191130 1400 台文冊展.jpg'),
+		    bangtsi: 'https://www.facebook.com/events/758580147888044/',
 		},
 	    {
 		    too: require('./too/20191130 1430 閉幕吳朋奉.png'),
-		    bangtsi: 'koooo',
+		    bangtsi: 'https://www.facebook.com/events/433300933829622/',
 		},
 	],
   },
