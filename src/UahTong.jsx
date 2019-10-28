414e9f612e6f5186071e5c7a1190e8e02c2e9b9a
Fafofafoy <ihcaoe@gmail.com>
2019-10-28 20:10:33 +0800

    Too ka bang-tsi

--- src/UahTong.jsx
+++ src/UahTong.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-const Too = ({tongmia}) => {
+const Too = ({tiunn}) => {
 return(
 	<div>
-	<a href="https://google.com" className="ui medium tsiatbok image">
-		<img src={tongmia} alt=''/>
+	<a href={tiunn.bangtsi} className="ui medium tsiatbok image">
+		<img src={tiunn.too} alt=''/>
 	</a>
 	</div>
 )}
@@ -14,7 +14,7 @@ const UahTong = ({sikan, hangbok}) => (
 	<div className="App-tsoo">
         <h1 className="ui inverted center aligned header">{sikan}</h1>
         <div className="App-flex-box">
-            {hangbok.map(too => <Too key={too} tongmia={too}/>)}
+            {hangbok.map(tiunn => <Too key={tiunn.too} tiunn={tiunn}/>)}
         </div>
     </div>
 )
