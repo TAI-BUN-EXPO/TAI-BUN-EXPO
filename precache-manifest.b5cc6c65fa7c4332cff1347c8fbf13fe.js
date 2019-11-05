c9dd77cf38a72217156d2b0930321473a8aa5252
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-05 15:57:06 +0000

    Deploy taibunexpo.taigi.info to github.com/TAI-BUN-EXPO/TAI-BUN-EXPO.git:gh-pages

---
+++ precache-manifest.b5cc6c65fa7c4332cff1347c8fbf13fe.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8efc3b934e74fe7c0d7e3f08fe92f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee20ea638589b7fb27d",
+    "url": "/static/css/main.96b06e0e.chunk.css"
+  },
+  {
+    "revision": "1e5228e557e8a95145a3",
+    "url": "/static/js/2.776b93c0.chunk.js"
+  },
+  {
+    "revision": "fee20ea638589b7fb27d",
+    "url": "/static/js/main.66ab5200.chunk.js"
+  },
+  {
+    "revision": "3b910554fb4572310f14",
+    "url": "/static/js/runtime-main.3e26e8d4.js"
+  },
+  {
+    "revision": "5e65a3fc080868b360bc05258f7f0479",
+    "url": "/static/media/20191101 1930 開幕陳奕齊.png.5e65a3fc.jpg"
+  },
+  {
+    "revision": "46137b8008941b8992319066ce520ff5",
+    "url": "/static/media/20191102 1430 來去鹽埕埔大溝頂行踏.png.46137b80.jpg"
+  },
+  {
+    "revision": "200394be9c3dd5c08a82d6d016db02b9",
+    "url": "/static/media/20191102 2000 大俠梅花鹿.png.200394be.jpg"
+  },
+  {
+    "revision": "d526f86e666e4088798b115dfec7fc9f",
+    "url": "/static/media/20191103 1500 古意唸歌團.png.d526f86e.jpg"
+  },
+  {
+    "revision": "4c60bdd1ee40edff20687046340dbed9",
+    "url": "/static/media/20191109 1430 台南妹仔周定邦.png.4c60bdd1.jpg"
+  },
+  {
+    "revision": "c3b1870a7d4dc601423adba478ad79cc",
+    "url": "/static/media/20191110 0900 來去旗津行踏.png.c3b1870a.jpg"
+  },
+  {
+    "revision": "5de068b2cbdbfa9b05b654f7fd3b4c54",
+    "url": "/static/media/20191110 1430 罐頭塔DIY.png.5de068b2.jpg"
+  },
+  {
+    "revision": "ceb144ea03a14a9878c7368115cc3f34",
+    "url": "/static/media/20191110 2000 拍謝少年維尼.png.ceb144ea.jpg"
+  },
+  {
+    "revision": "5e226e5fcd04edbf5ff0bccffb361b99",
+    "url": "/static/media/20191116 1000 林美美.png.5e226e5f.jpg"
+  },
+  {
+    "revision": "4c9f8b298fbda19b9cddd4a2206bcba6",
+    "url": "/static/media/20191116 1300 台語市仔.png.4c9f8b29.jpg"
+  },
+  {
+    "revision": "3827e80180ec7d3f12dd2685f1df4215",
+    "url": "/static/media/20191116 1930 魚仔林.png.3827e801.jpg"
+  },
+  {
+    "revision": "770ab37fc650b8db74642e9237e9d474",
+    "url": "/static/media/20191117 1430 王昭華.png.770ab37f.jpg"
+  },
+  {
+    "revision": "3ab89f996695846a8f8eae0d9bc03ea4",
+    "url": "/static/media/20191123 1100 趙子賢創作個展.3ab89f99.jpg"
+  },
+  {
+    "revision": "9d68d0e150f72b57978dd80aabfcf289",
+    "url": "/static/media/20191123 1430 你今仔日kám有講台語.png.9d68d0e1.jpg"
+  },
+  {
+    "revision": "b5304b5ece00c72a18eafb7b0ecc5c1a",
+    "url": "/static/media/20191124 0900 來去柴山.png.b5304b5e.jpg"
+  },
+  {
+    "revision": "0cedf872459b20eed0a21a663fd305e8",
+    "url": "/static/media/20191124 1430 港台垃圾話垃圾講.png.0cedf872.jpg"
+  },
+  {
+    "revision": "720f0c00f74b92d05595d8b7e06788ab",
+    "url": "/static/media/20191130 0900 來去哈馬星行踏.png.720f0c00.jpg"
+  },
+  {
+    "revision": "2b30d365942146f66a73c3155dbfe4aa",
+    "url": "/static/media/20191130 1400 台文冊展.2b30d365.jpg"
+  },
+  {
+    "revision": "69c32524b92549bc1bce6f5e178a2aa7",
+    "url": "/static/media/20191130 1430 閉幕吳朋奉.png.69c32524.jpg"
+  },
+  {
+    "revision": "eeca912ae640ce86607fb7f0d7f0bc5e",
+    "url": "/static/media/LOGO_NO_BG.579x584.eeca912a.png"
+  }
+]);
